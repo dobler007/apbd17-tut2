de0a705101d5fa1a19841f686789cb3017cd8048
dobler007 <A.drzhn@gmail.com>
2024-03-08 17:05:06 +0100

    add GetAverage method

--- apbd2/apbd2/Program.cs
+++ apbd2/apbd2/Program.cs
@@ -3,3 +3,14 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Mod 1");
+
+static double GetAverage(int[] nums)
+{
+    double sum = 0.0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        sum += nums[i];
+    }
+
+    return sum / nums.Length;
+}
